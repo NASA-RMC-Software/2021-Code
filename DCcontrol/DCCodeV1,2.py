baed8ec82058ac40dbacce4c6ce64ca6ff3a6cc9
Gabe Blaylock <gabemaster2001@gmail.com>
2020-10-15 15:34:41 -0400

    DC Motor control for raspberry pi

---
+++ DCcontrol/DCCodeV1,2.py
@@ -0,0 +1,35 @@
+import wiringpi as GPIO
+
+from gpiozero import PWMOutputDevice
+from time import sleep
+ 
+#///////////////// Define Motor Driver GPIO Pins /////////////////
+# Motor A, Left Side GPIO CONSTANTS
+PWM_FORWARD = 26	# IN1 - Forward Drive
+PWM_REVERSE = 19	# IN2 - Reverse Drive
+ 
+# Initialise objects for H-Bridge PWM pins
+# Set initial duty cycle to 0 and frequency to 1000
+forward = PWMOutputDevice(PWM_FORWARD, True, 0, 1000)
+reverse = PWMOutputDevice(PWM_REVERSE, True, 0, 1000)
+ 
+ 
+def allStop():
+	forward.value = 0
+	reverse.value = 0
+ 
+def forwardDrive(x):
+	forward.value = x
+	reverse.value = 0
+
+def reverseDrive(x):
+	forward.value = 0
+	reverse.value = x
+	
+def main():
+	allStop()
+	forwardDrive()
+	sleep(5)
+	reverseDrive()
+	sleep(5)
+	allStop()
